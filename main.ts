1e102d416716f5cb4519e8ce1cfb909d1231cca7
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-12-06 13:08:19 -0500

    SDg

--- main.ts
+++ main.ts
@@ -5,8 +5,6 @@
  * This program does bluetooth
 */
 
-// variable
-let distance: number = 0
 
 // setup
 basic.clearScreen()
@@ -14,24 +12,12 @@ radio.setGroup(1)
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
 
-while (true){
-  if (input.buttonIsPressed(Button.A) === true) {
-    basic.clearScreen()
-    // find distance
-    distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
-    if (distance < 6) {
-      radio.sendString('Too close! Move over!')
-      basic.showIcon(IconNames.Sword)
-    }
-    else {
-      radio.sendString('everything is A-Okay!')
-      basic.showIcon(IconNames.Happy)
-    }
-  }
 
+while (true){
+  // recieve message
   radio.onReceivedString(function (receivedString) {
     basic.clearScreen()
     basic.showString(receivedString)
-    basic.showIcon(IconNames.Surprised)
+    basic.showIcon(IconNames.Happy)
   })
 }
